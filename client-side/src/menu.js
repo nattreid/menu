b03cea111a096dc3c7e9f8b7a7088ffaf901d811
Attreid <attreid@gmail.com>
2016-06-21 14:27:22 +0200

    oprava js

--- client-side/src/menu.js
+++ client-side/src/menu.js
@@ -1,16 +1,22 @@
-/* ******************************** menu.js ********************************* */
+(function ($, window) {
+    if (window.jQuery === undefined) {
+        console.error('Plugin "jQuery" required by "menu.js" is missing!');
+        return;
+    }
 
-$(document).ready(function () {
+    $(document).ready(function () {
 
-    var time = 300;
+        var time = 300;
 
-    $('.menu-container dt a').click(function () {
-        var parent = $(this).closest('dl');
+        $('.menu-container dt a').click(function () {
+            var parent = $(this).closest('dl');
 
-        parent.children('dd').slideToggle(time, function () {
-            $(this).toggleClass('itemHidden');
+            parent.children('dd').slideToggle(time, function () {
+                $(this).toggleClass('itemHidden');
+            });
+
+            parent.children('dt').toggleClass('itemHidden');
         });
-
-        parent.children('dt').toggleClass('itemHidden');
     });
-});
+
+})(jQuery, window);
