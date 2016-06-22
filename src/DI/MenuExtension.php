e8883bc376ed19cd9704ee916c82eb14d8b544b4
Attreid <attreid@gmail.com>
2016-06-22 13:33:18 +0200

    uprava di

--- src/DI/MenuExtension.php
+++ src/DI/MenuExtension.php
@@ -19,19 +19,19 @@ class MenuExtension extends \Nette\DI\CompilerExtension {
 
         $builder = $this->getContainerBuilder();
 
-        $module = $builder->addDefinition($this->prefix('moduleMenu'))
+        $builder->addDefinition($this->prefix('moduleMenu'))
                 ->setImplement('\NAttreid\Menu\Module\IMenuFactory')
                 ->setFactory('\NAttreid\Menu\Module\Menu')
                 ->setArguments(['%namespace%'])
-                ->setAutowired(TRUE);
-        $module->addSetup('setMenu', [$config['items']]);
+                ->setAutowired(TRUE)
+                ->addSetup('setMenu', [$config['items']]);
 
-        $menu = $builder->addDefinition($this->prefix('menu'))
+        $builder->addDefinition($this->prefix('menu'))
                 ->setImplement('\NAttreid\Menu\Menu\IMenuFactory')
                 ->setFactory('\NAttreid\Menu\Menu\Menu')
                 ->setArguments(['%namespace%'])
-                ->setAutowired(TRUE);
-        $menu->addSetup('setMenu', [$config['items']]);
+                ->setAutowired(TRUE)
+                ->addSetup('setMenu', [$config['items']]);
 
         $builder->addDefinition($this->prefix('breadcrumb'))
                 ->setImplement('\NAttreid\Menu\Breadcrumb\IBreadcrumb')
