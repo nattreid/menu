487769156a375275acb099d3ae14d715443d28aa
Attreid <attreid@gmail.com>
2016-06-27 14:12:18 +0200

    update

--- src/DI/MenuExtension.php
+++ src/DI/MenuExtension.php
@@ -20,20 +20,20 @@ class MenuExtension extends \Nette\DI\CompilerExtension {
         $builder = $this->getContainerBuilder();
 
         $builder->addDefinition($this->prefix('moduleMenu'))
-                ->setImplement('\NAttreid\Menu\Module\IMenuFactory')
-                ->setFactory('\NAttreid\Menu\Module\Menu')
+                ->setImplement('NAttreid\Menu\Module\IMenuFactory')
+                ->setFactory('NAttreid\Menu\Module\Menu')
                 ->setArguments(['%namespace%'])
                 ->addSetup('setMenu', [$config['items']]);
 
         $builder->addDefinition($this->prefix('menu'))
-                ->setImplement('\NAttreid\Menu\Menu\IMenuFactory')
-                ->setFactory('\NAttreid\Menu\Menu\Menu')
+                ->setImplement('NAttreid\Menu\Menu\IMenuFactory')
+                ->setFactory('NAttreid\Menu\Menu\Menu')
                 ->setArguments(['%namespace%'])
                 ->addSetup('setMenu', [$config['items']]);
 
         $builder->addDefinition($this->prefix('breadcrumb'))
-                ->setImplement('\NAttreid\Menu\Breadcrumb\IBreadcrumb')
-                ->setFactory('\NAttreid\Menu\Breadcrumb\Breadcrumb');
+                ->setImplement('NAttreid\Menu\Breadcrumb\IBreadcrumb')
+                ->setFactory('NAttreid\Menu\Breadcrumb\Breadcrumb');
     }
 
 }
